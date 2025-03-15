ba8911072e2699a4812620b0b31ef5d300b1077e
Ayman Sedkey <aymansedkey10@gamil.com>
2025-03-15 17:47:54 +0200

    Edite the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer (){
             <footer className="bg-body-tertiary text-center text-lg-start" sticky="bottom">
             <div className="text-center p-3">
                 © 2020 Copyright:
-                <a className="text-body text-decoration-none fw-bold ms-2" href="https://www.linkedin.com/in/aymansedkey10/">Linkedin</a>
+                <a className="text-body text-decoration-none fw-bold ms-2" href="https://www.linkedin.com/in/aymansedkey10/">Ayman Sedkey</a>
             </div>
             </footer>
         </>
